fabb7b9cc30319186e3e99ccedb2d2c38eca9eb2
박태원 <pa6802@naver.com>
2019-10-16 15:55:45 +0900

    과제 2

--- hw1/HW/hw2.cpp
+++ hw1/HW/hw2.cpp
@@ -6,14 +6,16 @@
 int main(void)
 {
 	printf("섭씨온도를 소수로 3개를 입력하시오. \n");
-	float a, b, c;
-	scanf_s("%f %f %f", &a, &b, &c);
+	double a, b, c;
+	double d, e, f;
+	scanf_s("%lf %lf %lf", &a, &b, &c);
 
 
 	printf("화씨 온도는\n");
-	printf("%f\n", (9 / 5) * a + 32);
-	printf("%f\n", (9 / 5) * b + 32);
-	printf("%f\n", (9 / 5) * c + 32);
-	printf(" 이다");
+	d = (9.0 / 5.0) * a + 32.0;
+	e = (9.0 / 5.0) * b + 32.0;
+	f = (9.0 / 5.0) * c + 32.0;
+	printf("%.4f %.4f %.4f", d, e, f);
+	printf("입니다.");
 
 }
